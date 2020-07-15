bdb9a51dab4dc3fe6274a551bd1033912e6dc71f
Maiko <maikokuroki24@gmail.com>
2020-07-15 16:44:03 -0700

    unctrolled form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -73,6 +73,8 @@ class Contact extends Component {
     }
     
     render(){
+        const errors = this.validate(this.state.firstName, this.state.lastName, this.state.phoneNum, this.state.email);    
+
         return (
         
             <div className="container">
