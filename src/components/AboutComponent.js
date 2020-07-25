2e41cd94606adb41d2572f26adb5bd42fa8647b3
Maiko <maikokuroki24@gmail.com>
2020-07-24 17:05:41 -0700

    another update on About Component

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 
 function RenderPartner({partner}){
@@ -35,6 +36,28 @@ function About(props) {
            </Media>
         );
     });
+
+    if (props.partners.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.partners.errMess) {
+        return (
+            <div className="container">
+                <div className="row"> 
+                    <div className="col">
+                        <h4>{props.partners.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    } 
+
 
     return (
         <div className="container">
